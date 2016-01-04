26e0ea1d7cdd7461231ab7ee6bfdade0d3f4f814
Jason Hiatt <jason.hiatt@gmail.com>
2016-01-04 09:04:56 -0600

    Added script that loops through Avamar clients

---
+++ mass_delete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+THISDOMAIN="/"
+
+for LETTER in {a..z} 
+do
+        for CLIENT in $(mccli client show --domain=${THISDOMAIN}  | grep -i "^${LETTER}" | awk '{print $1}')
+        do 
+		##Skip the header
+                if [ "${CLIENT}" != "Client" ]
+                then
+			./delete_backups.sh "${THISDOMAIN}" "${CLIENT}"
+                fi
+        done
+done
+
