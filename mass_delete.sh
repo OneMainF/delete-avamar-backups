a2f4df47ad341a2b421759acf14c1ca2b8ae6b9e
Jason Hiatt <jason.hiatt@gmail.com>
2016-01-04 09:08:19 -0600

    Dev fix

--- mass_delete.sh
+++ mass_delete.sh
@@ -4,13 +4,14 @@ THISDOMAIN="/"
 
 for LETTER in {a..z} 
 do
-        for CLIENT in $(mccli client show --domain=${THISDOMAIN}  | grep -i "^${LETTER}" | awk '{print $1}')
-        do 
+	for CLIENT in $(mccli client show --domain=${THISDOMAIN}  | grep -i "^${LETTER}" | awk '{print $1}')
+	do 
 		##Skip the header
-                if [ "${CLIENT}" != "Client" ]
-                then
+		if [ "${CLIENT}" != "Client" ]
+		then
 			./delete_backups.sh "${THISDOMAIN}" "${CLIENT}"
-                fi
-        done
+		fi
+	done
 done
 
+exit 0
